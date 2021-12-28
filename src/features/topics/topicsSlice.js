ef9f10edaf486b1a145ca945ea1427918a08ee13
awarrington0895 <awarrington0895@gmail.com>
2021-12-28 18:00:37 +0000

    Add topics slice to global store

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const topicsSlice = createSlice({
+  name: 'topics',
+  initialState: {
+    topics: {}
+  },
+  reducers: {
+    addTopic: (state, { payload }) => {
+      state.topics[payload.id] = {...payload, quizIds: []}
+    }
+  }
+});
+
+export const selectTopics = state => state.topics;
+
+export const { addTopic } = topicsSlice.actions;
+
+export default topicsSlice.reducer;
